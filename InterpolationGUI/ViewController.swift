e120da6d47b1ec7d737a51c919fa3cbccbaa658f
Даниил Волошин <kuravasic@gmail.com>
2018-07-11 01:21:28 +0300

    Basic Interactions

--- InterpolationGUI/ViewController.swift
+++ InterpolationGUI/ViewController.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //Declaration of labels
+    @IBOutlet var label1: UILabel!
+    @IBOutlet var label2: UILabel!
+    
+    //Declaration of text boxes
+    @IBOutlet var textField1: UITextField!
+    @IBOutlet var textField2: UITextField!
+    @IBOutlet var textField3: UITextField!
+    @IBOutlet var textField4: UITextField!
+    @IBOutlet var textField5: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +31,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func button1Pressed(_ sender: Any) {
+        self.textField1.resignFirstResponder()
+        self.textField2.resignFirstResponder()
+        self.textField3.resignFirstResponder()
+        self.textField4.resignFirstResponder()
+        self.textField5.resignFirstResponder()
+        label2.text = "x = \(textField5.text ?? "")"
+    }
+    
+    
+    //If you press anywhere else, removes all the extra UI (keyboard, droplist, etc.)
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 
 
 }
